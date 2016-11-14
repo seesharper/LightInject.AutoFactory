9ce51e57750d3bbe5715555a41ba286305ac91e2
Bernhard Richter <bernhard.richter@gmail.com>
2016-11-14 14:52:16 +0100

    Bumped to 1.1.1

--- src/LightInject.AutoFactory/LightInject.AutoFactory.cs
+++ src/LightInject.AutoFactory/LightInject.AutoFactory.cs
@@ -21,7 +21,7 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 ******************************************************************************
-    LightInject.AutoFactory version 1.1.0
+    LightInject.AutoFactory version 1.1.1
     http://www.lightinject.net/
     http://twitter.com/bernhardrichter
 ******************************************************************************/
